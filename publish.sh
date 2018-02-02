8d2b883cc432294a8c47386de5d9b4b6637b2b46
Matthias Gernand <m.gernand@fluxera.com>
2018-02-02 15:28:59 +0100

    Fixed deploy script.

--- publish.sh
+++ publish.sh
@@ -1,5 +1,13 @@
 if [ ! -s "$TRAVIS_TAG" ] ; then 
 	docker tag $DOCKER_HUB_ORG/brisk-controller:latest-dev $DOCKER_HUB_ORG/brisk-controller:$TRAVIS_TAG;
 	docker login -u=$DOCKER_HUB_USERNAME -p=$DOCKER_HUB_PASSWORD;
-	docker push $DOCKER_HUB_ORG/gateway:$TRAVIS_TAG;
+	docker push $DOCKER_HUB_ORG/brisk-controller:$TRAVIS_TAG;
+
+	docker tag $DOCKER_HUB_ORG/brisk-gateway:latest-dev $DOCKER_HUB_ORG/brisk-gateway:$TRAVIS_TAG;
+	docker login -u=$DOCKER_HUB_USERNAME -p=$DOCKER_HUB_PASSWORD;
+	docker push $DOCKER_HUB_ORG/brisk-gateway:$TRAVIS_TAG;
+
+	docker tag $DOCKER_HUB_ORG/brisk-queue:latest-dev $DOCKER_HUB_ORG/brisk-queue:$TRAVIS_TAG;
+	docker login -u=$DOCKER_HUB_USERNAME -p=$DOCKER_HUB_PASSWORD;
+	docker push $DOCKER_HUB_ORG/brisk-queue:$TRAVIS_TAG;
 fi
