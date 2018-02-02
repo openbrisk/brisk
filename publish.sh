081323e7f77ffe53f5cf9b427134db714c0614c0
Matthias Gernand <m.gernand@fluxera.com>
2018-02-02 16:01:33 +0100

    Used --password-stdin for docker login.

--- publish.sh
+++ publish.sh
@@ -1,13 +1,10 @@
 if [ ! -s "$TRAVIS_TAG" ] ; then 
-	docker tag $DOCKER_HUB_ORG/brisk-controller:latest-dev $DOCKER_HUB_ORG/brisk-controller:$TRAVIS_TAG;
-	docker login -u=$DOCKER_HUB_USERNAME -p=$DOCKER_HUB_PASSWORD;
-	docker push $DOCKER_HUB_ORG/brisk-controller:$TRAVIS_TAG;
-
-	docker tag $DOCKER_HUB_ORG/brisk-gateway:latest-dev $DOCKER_HUB_ORG/brisk-gateway:$TRAVIS_TAG;
-	docker login -u=$DOCKER_HUB_USERNAME -p=$DOCKER_HUB_PASSWORD;
-	docker push $DOCKER_HUB_ORG/brisk-gateway:$TRAVIS_TAG;
-
-	docker tag $DOCKER_HUB_ORG/brisk-queue:latest-dev $DOCKER_HUB_ORG/brisk-queue:$TRAVIS_TAG;
-	docker login -u=$DOCKER_HUB_USERNAME -p=$DOCKER_HUB_PASSWORD;
-	docker push $DOCKER_HUB_ORG/brisk-queue:$TRAVIS_TAG;
+	docker tag $DOCKER_HUB_ORG/brisk-controller:latest-dev $DOCKER_HUB_ORG/brisk-controller:$TRAVIS_TAG
+	docker tag $DOCKER_HUB_ORG/brisk-queue:latest-dev $DOCKER_HUB_ORG/brisk-queue:$TRAVIS_TAG
+	docker tag $DOCKER_HUB_ORG/brisk-gateway:latest-dev $DOCKER_HUB_ORG/brisk-gateway:$TRAVIS_TAG
+	echo $DOCKER_HUB_PASSWORD | docker login -u=$DOCKER_HUB_USERNAME --password-stdin
+	docker push $DOCKER_HUB_ORG/brisk-controller:$TRAVIS_TAG
+	docker push $DOCKER_HUB_ORG/brisk-gateway:$TRAVIS_TAG
+	docker push $DOCKER_HUB_ORG/brisk-queue:$TRAVIS_TAG
+	docker logout
 fi
